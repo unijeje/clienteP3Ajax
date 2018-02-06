6053a4786778d6a533d09e29b7c4d54cec52d6c0
unijeje <veturn@hotmail.com>
2018-02-06 11:29:16 +0100

    sql

---
+++ clientep3_autobuses.sql
@@ -0,0 +1,192 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-02-2018 a las 11:17:54
+-- Versión del servidor: 10.1.28-MariaDB
+-- Versión de PHP: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `clientep3_autobuses`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alquiler`
+--
+
+CREATE TABLE `alquiler` (
+  `id` int(9) NOT NULL,
+  `horas` int(3) NOT NULL,
+  `fecha` date NOT NULL,
+  `numpersonas` int(3) NOT NULL,
+  `descripcion` text COLLATE utf8_spanish_ci NOT NULL,
+  `origen` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `destino` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `kms` int(4) NOT NULL,
+  `cliente` varchar(9) COLLATE utf8_spanish_ci NOT NULL,
+  `matricula_autobus` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `dni_conductor` varchar(9) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autobus`
+--
+
+CREATE TABLE `autobus` (
+  `matricula` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `asientos` int(3) NOT NULL,
+  `modelo` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `consumo` int(5) NOT NULL,
+  `itv` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `dni` varchar(9) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
+  `apellidos` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` int(9) NOT NULL,
+  `correo` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `sexo` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `conductor`
+--
+
+CREATE TABLE `conductor` (
+  `dni` varchar(9) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `apellidos` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `sexo` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` int(9) NOT NULL,
+  `email` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `direccion` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mantenimiento`
+--
+
+CREATE TABLE `mantenimiento` (
+  `id` int(9) NOT NULL,
+  `matricula_autobus` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish_ci NOT NULL,
+  `importe` float NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vacaciones`
+--
+
+CREATE TABLE `vacaciones` (
+  `id` int(9) NOT NULL,
+  `dni_conductor` varchar(9) COLLATE utf8_spanish_ci NOT NULL,
+  `fecha_ini` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `descripcion` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alquiler`
+--
+ALTER TABLE `alquiler`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cliente` (`cliente`),
+  ADD KEY `fk_conductor` (`dni_conductor`),
+  ADD KEY `fk_autobus` (`matricula_autobus`);
+
+--
+-- Indices de la tabla `autobus`
+--
+ALTER TABLE `autobus`
+  ADD PRIMARY KEY (`matricula`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`dni`);
+
+--
+-- Indices de la tabla `conductor`
+--
+ALTER TABLE `conductor`
+  ADD PRIMARY KEY (`dni`);
+
+--
+-- Indices de la tabla `mantenimiento`
+--
+ALTER TABLE `mantenimiento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `matricula_autobus` (`matricula_autobus`);
+
+--
+-- Indices de la tabla `vacaciones`
+--
+ALTER TABLE `vacaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dni_conductor` (`dni_conductor`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alquiler`
+--
+ALTER TABLE `alquiler`
+  ADD CONSTRAINT `alquiler_ibfk_1` FOREIGN KEY (`cliente`) REFERENCES `cliente` (`dni`),
+  ADD CONSTRAINT `fk_autobus` FOREIGN KEY (`matricula_autobus`) REFERENCES `autobus` (`matricula`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_conductor` FOREIGN KEY (`dni_conductor`) REFERENCES `conductor` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `mantenimiento`
+--
+ALTER TABLE `mantenimiento`
+  ADD CONSTRAINT `mantenimiento_ibfk_1` FOREIGN KEY (`matricula_autobus`) REFERENCES `autobus` (`matricula`);
+
+--
+-- Filtros para la tabla `vacaciones`
+--
+ALTER TABLE `vacaciones`
+  ADD CONSTRAINT `vacaciones_ibfk_1` FOREIGN KEY (`dni_conductor`) REFERENCES `conductor` (`dni`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
