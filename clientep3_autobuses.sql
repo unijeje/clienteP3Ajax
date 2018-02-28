0ccc68d7b465651c2988021d9b9b0fa018bc8817
unijeje <veturn@hotmail.com>
2018-02-28 20:05:12 +0100

    sql final

--- clientep3_autobuses.sql
+++ clientep3_autobuses.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-02-2018 a las 18:44:27
+-- Tiempo de generación: 28-02-2018 a las 19:53:15
 -- Versión del servidor: 5.5.27
 -- Versión de PHP: 5.4.7
 
@@ -19,7 +19,9 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `clientep3_autobuses`
 --
-
+DROP DATABASE IF EXISTS clientep3_autobuses;
+CREATE DATABASE clientep3_autobuses;
+USE clientep3_autobuses;
 -- --------------------------------------------------------
 
 --
@@ -51,7 +53,10 @@ CREATE TABLE IF NOT EXISTS `alquiler` (
 --
 
 INSERT INTO `alquiler` (`id`, `horas`, `fecha`, `numpersonas`, `descripcion`, `origen`, `destino`, `kms`, `cliente`, `matricula_autobus`, `dni_conductor`, `localidad`, `estado`) VALUES
-(1111, 5, '2018-02-28', 5, 'comentario', 'origen', 'destino', 5, '12345678A', '1234TTT', '49032440M', 'Camas', 1);
+(123, 8, '2018-05-10', 110, 'Excursion', 'Colegio', 'Zoo', 30, '98765432Z', '1234BBB', '09876098Z', 'Sevilla', 1),
+(759, 20, '2018-02-24', 45, 'VIAJE', 'ESPAÑA', 'FRANCIA', 500, '12345678Z', '3857KSH', '09872543G', 'San Juan de Aznalfarache', 1),
+(955, 4, '2018-02-06', 45, 'Excursion', 'Colegio', 'Teatro', 10, '98765498F', '4321ZAF', '09876589K', 'Dos Hermanas', 1),
+(973, 6, '2018-02-21', 25, 'Viaje', 'Sevilla', 'Cadiz', 80, '12345678Z', '3297PQE', '09876098Z', 'Sevilla', 0);
 
 -- --------------------------------------------------------
 
@@ -74,7 +79,11 @@ CREATE TABLE IF NOT EXISTS `autobus` (
 --
 
 INSERT INTO `autobus` (`matricula`, `asientos`, `modelo`, `consumo`, `itv`, `estado`) VALUES
-('1234TTT', 35, 'LOOOL', 5, 1, 0);
+('1234BBB', 30, 'Mercedes-Benz', 11, 1, 1),
+('1234TTT', 30, 'prueba', 5, 1, 0),
+('3297PQE', 40, 'Setra', 13, 0, 1),
+('3857KSH', 45, 'Scania', 15, 0, 1),
+('4321ZAF', 35, 'Tussam', 11, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -98,11 +107,11 @@ CREATE TABLE IF NOT EXISTS `cliente` (
 --
 
 INSERT INTO `cliente` (`dni`, `nombre`, `apellidos`, `telefono`, `correo`, `sexo`, `estado`) VALUES
-('12345678A', 'nombreMODIFICADO', 'ape', 975757575, 'ccror2@gmail.com', 'Femenino', 1),
-('12345678H', 'PEPE', 'PEPE', 955555555, 'correo@gmail.es', 'Masculino', 1),
-('12345678k', 'nombre', 'ape', 955555555, 'corrr@gmail.co', 'Masculino', 1),
-('12345678Q', 'nombres', 'apes', 955555555, 'corrr@gmail.com', 'Masculino', 1),
-('98765432F', 'Test', 'apeTest', 950000000, 'corr@gmail.es', 'Femenino', 1);
+('12345678A', 'Aabraham', 'Arriola', 688452541, 'Correo001@gmail.com', 'Masculino', 0),
+('12345678Z', 'Aada', 'Aldana', 688452555, 'Correo002@gmail.com', 'Femenino', 1),
+('98765432A', 'Javier', 'Bermudez', 681245245, 'Correo031@gmail.com', 'Masculino', 1),
+('98765432Z', 'Miguel', 'Benitez', 681245097, 'Correo101@gmail.com', 'Masculino', 1),
+('98765498F', 'Carlos', 'Costa', 681240097, 'Correo501@gmail.com', 'Masculino', 1);
 
 -- --------------------------------------------------------
 
@@ -127,8 +136,11 @@ CREATE TABLE IF NOT EXISTS `conductor` (
 --
 
 INSERT INTO `conductor` (`dni`, `nombre`, `apellidos`, `sexo`, `telefono`, `email`, `direccion`, `estado`) VALUES
-('49032440M', 'Alejandro', 'Nunez', 'Femenino', 954120369, 'ale@gmail.com', 'C/ Adriano', 1),
-('59896936E', 'Aitor', 'Menta', 'Masculino', 658969874, 'rayosycentellas@gmail.com', 'C/ Antonio Banderas', 0);
+('09872543G', 'Pepe', 'De leon', 'Masculino', 682552541, 'Correo052@gmail.com', 'C/php', 1),
+('09876098Z', 'Maria', 'Espinosa', 'Femenino', 688092541, 'Correo001@gmail.com', 'C/java', 1),
+('09876509P', 'Pablo', 'Etxebarria', 'Masculino', 688453141, 'Correo071@gmail.com', 'C/python', 0),
+('09876543F', 'Julio', 'Cuellar', 'Masculino', 680752541, 'Correo749@gmail.com', 'C/javascript', 1),
+('09876589K', 'Cristina', 'Flores', 'Femenino', 688452001, 'Correo571@gmail.com', 'C/C  ', 1);
 
 -- --------------------------------------------------------
 
@@ -168,7 +180,15 @@ CREATE TABLE IF NOT EXISTS `mantenimiento` (
   `estado` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `matricula_autobus` (`matricula_autobus`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=27 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `mantenimiento`
+--
+
+INSERT INTO `mantenimiento` (`id`, `matricula_autobus`, `descripcion`, `importe`, `fecha`, `estado`) VALUES
+(1, '1234BBB', 'Pintar', 105, '2018-02-14', 1),
+(2, '4321ZAF', 'Fallo de motor', 500, '2018-01-16', 0);
 
 -- --------------------------------------------------------
 
@@ -185,7 +205,15 @@ CREATE TABLE IF NOT EXISTS `vacaciones` (
   `estado` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `dni_conductor` (`dni_conductor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `vacaciones`
+--
+
+INSERT INTO `vacaciones` (`id`, `dni_conductor`, `fecha_ini`, `fecha_fin`, `descripcion`, `estado`) VALUES
+(1, '09872543G', '2018-02-28', '2018-03-31', 'VIAJE', 0),
+(2, '09876098Z', '2018-02-09', '2018-02-22', 'Enfermedad', 1);
 
 --
 -- Restricciones para tablas volcadas
